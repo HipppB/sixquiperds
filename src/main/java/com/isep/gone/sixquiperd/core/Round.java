46a2becdbdd2e1b0a02f7b5b680638151b4160ca
Adeline Henrion <ahenrion@juniorisep.com>
2023-05-26 18:51:43 +0200

    feat(round): init round

--- src/main/java/com/isep/gone/sixquiperd/core/Round.java
+++ src/main/java/com/isep/gone/sixquiperd/core/Round.java
@@ -1,13 +1,33 @@
 package com.isep.gone.sixquiperd.core;
 
-import java.util.List;
+import java.util.*;
 
 public class Round {
     List<Player> players;
+    Deque<Card> deck;
     Board board;
 
-    public Round() {
+    public Round(List<Player>players, List<Card> initialDeck) {
         this.players = players;
-        this.board = board;
+        Collections.shuffle(initialDeck);
+        this.deck = new ArrayDeque<>(initialDeck);
+        giveCardsToPlayers();
+        this.board = new Board(deck);
+    }
+
+    private void giveCardsToPlayers() {
+        players.forEach(player -> {
+            List<Card> playerCards = new ArrayList<>();
+            for (int i = 0; i<10; i++){
+                playerCards.add(deck.remove());
+            }
+            player.initHand(playerCards);
+        });
+    }
+
+    public void play() {
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
