df58148e1018457b9ec4d4537626b18603f55711
Adeline Henrion <ahenrion@juniorisep.com>
2023-05-26 18:49:50 +0200

    create all cards

--- src/main/java/com/isep/gone/sixquiperd/core/Game.java
+++ src/main/java/com/isep/gone/sixquiperd/core/Game.java
@@ -1,21 +1,35 @@
 package com.isep.gone.sixquiperd.core;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
     List<Player> players;
-    List<Card> deck;
-    List<Card> initialDeck;
+    List<Card> initialDeck = new ArrayList<>();
 
     Round currentRound;
 
     public Game() {
         initDeck();
-        play();
+//        play();
     }
 
     private void initDeck() {
-
+        for (int i = 1; i <= 104; i++){
+            int beefHead = 0;
+            if(i%11 == 0){
+                beefHead += 5;
+            }
+            if(i%10 == 0){
+                beefHead += 3;
+            } else if(i%5 == 0) {
+                beefHead += 2;
+            }
+            if(beefHead == 0) {
+                beefHead = 1;
+            }
+            initialDeck.add(new Card(i, beefHead));
+        }
     }
 
     private void play() {
