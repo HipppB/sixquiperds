fbc6a60a2da52a10337d41df6755f0d722e0dd8e
Adeline Henrion <ahenrion@juniorisep.com>
2023-05-27 01:39:45 +0200

    feat(board): init and add card to board

--- src/main/java/com/isep/gone/sixquiperd/core/Board.java
+++ src/main/java/com/isep/gone/sixquiperd/core/Board.java
@@ -1,26 +1,52 @@
 package com.isep.gone.sixquiperd.core;
 
 import lombok.Data;
+import lombok.NonNull;
 
 import java.util.*;
 
 @Data
 public class Board {
-    ArrayDeque<Card>[] rows = new ArrayDeque[4];
+    @NonNull List<Deque<Card>> rows = new ArrayList<>();
+
     public Board(Deque<Card> cards) {
-        if(cards.size() < 4){
+        if (cards.size() < 4) {
             throw new IllegalArgumentException("Cannot play with less than 4 cards !");
         }
-        for (var row: rows){
-            row.add(cards.remove());
+        for (int i = 0; i < 4; i++) {
+            Deque<Card> deque = new ArrayDeque<>();
+            deque.add(cards.remove());
+            rows.add(deque);
         }
     }
 
-    public boolean addCardToBoard(Card card) {
-        return false;
+    public boolean addCardToBoard(Card card, Player player) {
+        var usableRows = new ArrayList<>(this.rows.stream().filter(row -> {
+            assert row.peek() != null;
+            return row.peek().cardNumber < card.cardNumber;
+        }).toList());
+        if (usableRows.isEmpty()) {
+            return false;
+        }
+        usableRows.sort(Comparator.comparingInt(o -> o.peek().cardNumber));
+        Deque<Card> row = rows.get(rows.size() - 1);
+        int rowNumber = this.rows.indexOf(row);
+        if (row.size() == 5) {
+            player.addScore(row.stream().mapToInt(card1 -> card1.beefHead).sum());
+            row = new ArrayDeque<>();
+        }
+        row.add(card);
+        this.rows.set(rowNumber, row);
+        return true;
     }
 
-    public boolean addCardToBoard(Card card, int rowNumber){
-        return false;
+    public void addCardToBoardRow(Card card, int rowNumber, Player player) {
+        Deque<Card> row = rows.get(rowNumber);
+        if (row.size() == 5) {
+            player.addScore(row.stream().mapToInt(card1 -> card1.beefHead).sum());
+            row = new ArrayDeque<>();
+        }
+        row.add(card);
+        rows.set(rowNumber, row);
     }
 }
